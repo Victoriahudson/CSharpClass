baac28022c386ce17e3f1d946f10b6bdff6f39bb
Victoriahudson <vickih287@gmail.com>
2019-09-26 10:56:53 -0500

    Lesson 7 Operators

--- Program.cs
+++ Program.cs
@@ -12,21 +12,34 @@ namespace CSharpClass
 
             //Console.WriteLine("Hello World!");
             //MyLesson5examples();
-            MyLesson6Examples();
+            //MyLesson6Examples();
+            MyLesson7Examples();
         } // End Method Main
 
-
+        static void MyLesson7Examples()
+        {
+            Lesson7 mySeven = new Lesson7();
+            mySeven.BasicMath();
+            mySeven.BasicModulus();
+            mySeven.MyCheckOperator1();
+            mySeven.MyCheckOperator2();
+            mySeven.MyLeftShiftOperator();
+            mySeven.MyAndAssignmentOperator();
+            mySeven.MyOrAssignmentOperator();
+            mySeven.MyXOrAssignmentOperator();
+            mySeven.MyIncrementDecrementExample();
+        }  // end method my lesson 7 examples
         static void MyLesson6Examples()
         {
             Lesson6 mySix = new Lesson6();
-                mySix.MyTrimExample();
-                mySix.MyEqualsExample();
-                mySix.myLowerUpperExample();
-                mySix.MyStringLength();
-                string myJoined = mySix.MyJoinedStrings("Happy","Coding");
-                Console.WriteLine(myJoined);
-                string myFav = mySix.MyStringBuilder("pizza", 3); 
-                Console.WriteLine(myFav);
+            mySix.MyTrimExample();
+            mySix.MyEqualsExample();
+            mySix.myLowerUpperExample();
+            mySix.MyStringLength();
+            string myJoined = mySix.MyJoinedStrings("Happy", "Coding");
+            Console.WriteLine(myJoined);
+            string myFav = mySix.MyStringBuilder("pizza", 3);
+            Console.WriteLine(myFav);
 
             // end method my lesson 6 examples
         }
